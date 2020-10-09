aa10c0332e039039e411e1f01fca90763f199a38
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-10-08 22:15:04 -0500

    Add files via upload

---
+++ ReverseInt.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 24 23:32:05 2020
+
+@author: Shipra
+"""
+
+num = 9812
+
+class Solution:
+    def reverseInt(self, num):
+        
+
+sol = Solution()
+print(sol.reverseInt(num))
