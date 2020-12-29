f5a35071561f6220dae763217dc49e8968e1e859
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-12-29 12:08:15 -0600

    Add files via upload

---
+++ Arrays.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 15:39:04 2020
+
+@author: Shipra Saini
+"""
+
+arr = [1,2,3,4,5,6,7,8]
+d = input("Enter value of d: ")
+n = input("Enter value of n: ")
